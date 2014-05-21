5aa62d65c0335f295e0339e96bc4ad3a7aba987c
NIcolas Grangier <n.grangier2@gmail.com>
2014-05-21 17:42:18 -0400

    changes

--- apache2/recipes/chown.rb
+++ apache2/recipes/chown.rb
@@ -1,5 +1,10 @@
-directory "/srv/www/test_git/current" do
-  owner "apache"
-  group "apache"
-  recursive true
+%w[ "/srv/www/test_git/current/**/*" ].each do |path|
+  file path do
+    owner "apache"
+    group "apache"
+  end if File.file?(path)
+  directory path do
+    owner "apache"
+    group "apache"
+  end if File.directory?(path)
 end
