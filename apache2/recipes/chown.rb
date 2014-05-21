0e791dc0404f5b2a3378d5c2fe140ff03bdffd5d
NIcolas Grangier <n.grangier2@gmail.com>
2014-05-21 17:31:31 -0400

    changes : end

--- apache2/recipes/chown.rb
+++ apache2/recipes/chown.rb
@@ -1,10 +1,5 @@
-["/srv",
- "/srv/www",
- "/srv/www/test_git"].each do |dir|
-    directory dir do
-      owner "apache"
-      group "apache"
-      action :create
-      recursive true
-  end
+directory "/srv/www/test_git/current" do
+  owner "apache"
+  group "apache"
+  recursive true
 end
