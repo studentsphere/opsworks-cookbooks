ded05af77e4e28e4e012e4f9e95733f8130c4cdc
Nicolas Grangier <n.grangier2@gmail.com>
2014-08-19 15:43:40 -0400

    delete-docker-files-prod

---
+++ docker/recipes/delete-docker-files-prod.rb
@@ -0,0 +1,18 @@
+file "/srv/www/drupalmaster/current/dockerconfig/*" do
+  action :delete
+end
+file "/srv/www/drupalmaster/current/automation" do
+  action :delete
+end
+file "/srv/www/drupalmaster/current/Dockerfile" do
+  action :delete
+end
+file "/srv/www/drupalmaster/current/start.sh" do
+  action :delete
+end
+file "/srv/www/drupalmaster/current/startinsidedocker.sh" do
+  action :delete
+end
+file "/srv/www/drupalmaster/current/supervisord.conf" do
+  action :delete
+end
