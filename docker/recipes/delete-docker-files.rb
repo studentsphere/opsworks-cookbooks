6ddc65dae1b5b947512a0a534b8f43a07741d3c1
Nicolas Grangier <n.grangier2@gmail.com>
2014-08-19 15:01:48 -0400

    Create a recipe for delete-docker-files ENV stag

---
+++ docker/recipes/delete-docker-files.rb
@@ -0,0 +1,20 @@
+file "/srv/www/drupalstaging/current/dockerconfig/*" do
+  action :delete
+end
+file "/srv/www/drupalstaging/current/automation" do
+  action :delete
+end
+file "/srv/www/drupalstaging/current/Dockerfile" do
+  action :delete
+end
+file "/srv/www/drupalstaging/current/start.sh" do
+  action :delete
+end
+file "/srv/www/drupalstaging/current/startinsidedocker.sh" do
+  action :delete
+end
+
+file "/srv/www/drupalstaging/current/supervisord.conf
+" do
+  action :delete
+end
