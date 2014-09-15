15bd738091bc1682f494d83f5dc1375f57c30752
Nicolas Grangier <n.grangier2@gmail.com>
2014-09-15 18:35:43 -0400

    Locale Universal

--- php/recipes/locale.rb
+++ php/recipes/locale.rb
@@ -1,7 +1,7 @@
 script "append-custom-apc" do
   interpreter "bash"
   user "root"
-  cwd "/srv/www/drupalmaster/current"
+  cwd "/srv/www"
   code <<-EOH
   locale-gen fr_FR && locale-gen fr_FR.utf8 
   EOH
